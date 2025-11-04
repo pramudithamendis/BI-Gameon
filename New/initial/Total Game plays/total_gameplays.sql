354bfbc59f32bb39620a501fb7f553cfa4c88df1
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-04 17:38:19 +0530

    additions

--- New/initial/Total Game plays/total_gameplays.sql
+++ New/initial/Total Game plays/total_gameplays.sql
@@ -5,8 +5,9 @@ CREATE TABLE total_game_plays_total (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-select * from total_game_plays_total;
+drop table total_game_plays_total;
 
+select * from total_game_plays_total;
 SET @cutoff := '2025-09-27 18:30:00';
 insert into total_game_plays_total(total_sessions)
 SELECT 
@@ -16,22 +17,29 @@ JOIN gaming_app_backend.user_game_session ugs
 ON gs.id = ugs.game_session
 where gs.created_at >= @cutoff;
 
--- CREATE TABLE total_game_plays_monthly (
---     id BIGINT AUTO_INCREMENT PRIMARY KEY,
---     month_year VARCHAR(7) NOT NULL,   -- format: YYYY-MM
---     total_sessions INT NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+CREATE TABLE total_game_plays_monthly (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    month_ VARCHAR(7) NOT NULL,   -- format: YYYY-MM
+    total_sessions INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 
--- insert into total_game_plays_monthly("",total_sessions)
--- SELECT 
---     COUNT(DISTINCT gs.id) AS total_sessions
--- FROM gaming_app_backend.game_session gs
--- JOIN gaming_app_backend.user_game_session ugs
---     ON gs.id = ugs.game_session
--- WHERE YEAR(gs.created_at) = YEAR(CURDATE())   
---   AND MONTH(gs.created_at) = MONTH(CURDATE()); 
+select * from total_game_plays_monthly;
+
+SET @cutoff := '2025-09-27 18:30:00';
+insert into total_game_plays_monthly(month_,total_sessions)
+SELECT 
+    DATE_FORMAT(gs.created_at, '%Y-%m') AS month_,
+    COUNT(DISTINCT gs.id) AS total_sessions
+FROM gaming_app_backend.game_session gs
+JOIN gaming_app_backend.user_game_session ugs
+    ON gs.id = ugs.game_session
+WHERE gs.created_at >= @cutoff
+GROUP BY DATE_FORMAT(gs.created_at, '%Y-%m')
+ORDER BY month_;
+
+
 
 
 CREATE TABLE total_game_plays_weekly (
