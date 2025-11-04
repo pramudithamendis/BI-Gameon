55c8d53ea2f7549bf0c1cf753c71a4b8e7bf73ff
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-04 16:39:58 +0530

    fixes for TotalGamePlayswithoutAIandTrainWithAI

--- New/initial/Total Game Plays without AI and Train With AI/TotalGamePlayswithoutAIandTrainWithAI.sql
+++ New/initial/Total Game Plays without AI and Train With AI/TotalGamePlayswithoutAIandTrainWithAI.sql
@@ -1,19 +1,19 @@
 CREATE TABLE Total_Game_Plays_without_AI_and_Train_With_AI (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    date_ DATE NOT NULL,
     total_sessions INT NOT NULL DEFAULT 0,
-    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE KEY unique_date (date_)
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+drop table Total_Game_Plays_without_AI_and_Train_With_AI;
+select * from Total_Game_Plays_without_AI_and_Train_With_AI;
 
-insert into Total_Game_Plays_without_AI_and_Train_With_AI(date_, total_sessions)
+insert into Total_Game_Plays_without_AI_and_Train_With_AI(total_sessions)
 SELECT 
-    gs.created_at as date_
     COUNT(DISTINCT gs.id) AS total_sessions
 FROM gaming_app_backend.game_session gs
 JOIN gaming_app_backend.user_game_session ugs 
     ON gs.id = ugs.game_session
 WHERE 
     gs.created_at > '2025-09-27'
-    AND gs.game_session_mode NOT IN (3, 5);
+    AND gs.game_session_mode NOT IN (3, 5)
+
