aa8abfbba2150dceeaca66e959b3a21e0bd991a5
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-07 13:56:03 +0530

    totalGamePlayrsWithoutAIandTrainWithAI

--- New/incremental/4 Total Game Plays without AI and Train With AI/TotalGamePlayswithoutAIandTrainWithAI.sql
+++ New/incremental/4 Total Game Plays without AI and Train With AI/TotalGamePlayswithoutAIandTrainWithAI.sql
@@ -5,18 +5,18 @@ select * from Total_Game_Plays_without_AI_and_Train_With_AI;
 USE gaming_app_bi;
 
 -- Get yesterday's date in Singapore timezone (+08:00)
-SET @yesterday := DATE(CONVERT_TZ(DATE_SUB(NOW(), INTERVAL 1 DAY), '+00:00', '+08:00'));
+-- SET @yesterday := DATE(CONVERT_TZ(DATE_SUB(NOW(), INTERVAL 1 DAY), '+00:00', '+08:00'));
 
-INSERT INTO Total_Game_Plays_without_AI_and_Train_With_AI (date, total_sessions)
-SELECT 
-    @yesterday AS date,
-    COUNT(DISTINCT gs.id) AS total_sessions
-FROM gaming_app_backend.game_session gs
-JOIN gaming_app_backend.user_game_session ugs 
-    ON gs.id = ugs.game_session
-WHERE 
-    DATE(CONVERT_TZ(gs.created_at, '+00:00', '+08:00')) = @yesterday
-    AND gs.game_session_mode NOT IN (3, 5)
-ON DUPLICATE KEY UPDATE 
-    total_sessions = VALUES(total_sessions),
-    updated_at = CURRENT_TIMESTAMP;
+-- INSERT INTO Total_Game_Plays_without_AI_and_Train_With_AI (date, total_sessions)
+-- SELECT 
+--     @yesterday AS date,
+--     COUNT(DISTINCT gs.id) AS total_sessions
+-- FROM gaming_app_backend.game_session gs
+-- JOIN gaming_app_backend.user_game_session ugs 
+--     ON gs.id = ugs.game_session
+-- WHERE 
+--     DATE(CONVERT_TZ(gs.created_at, '+00:00', '+08:00')) = @yesterday
+--     AND gs.game_session_mode NOT IN (3, 5)
+-- ON DUPLICATE KEY UPDATE 
+--     total_sessions = VALUES(total_sessions),
+--     updated_at = CURRENT_TIMESTAMP;
