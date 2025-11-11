28eb4f2b2d6b6948b178d1cfbfb3c9d5db0528e1
pramudithamendis <134496244+pramudithamendis@users.noreply.github.com>
2025-11-11 15:26:14 +0530

    incremental of withdrawals both whole

---
+++ 3 New using uct table/incremental/2b withdrawals both whole/withdrawals both whole.sql
@@ -0,0 +1,93 @@
+
+-- user_coin_transaction_method
+	-- 3 withdrawal
+    -- 4 fiat-deposit
+    -- 5 crypto-deposit
+    -- 9 crypto-withdrawal
+
+drop table uct_total_withdrawals_both_daily;
+
+select * from uct_total_withdrawals_both_daily;
+SET @yesterday := DATE(CONVERT_TZ(DATE_SUB(NOW(), INTERVAL 1 DAY), '+00:00', '+08:00'));
+ 
+SET @cutoff := '2025-09-27 18:30:00';
+insert into uct_total_withdrawals_both_daily(date_,total_completed_amount, total_transactions)
+SELECT 
+	   DATE(w.created_at) AS date_,
+	   SUM(w.coins) as total_completed_coins,
+	    COUNT(*) AS total_transactions
+FROM gaming_app_backend.user_coin_transaction w
+JOIN gaming_app_backend.user u ON w.user = u.id
+WHERE w.created_at >= @cutoff and (w.user_coin_transaction_method = 3 or w.user_coin_transaction_method = 9)
+ AND DATE(CONVERT_TZ(w.created_at, '+00:00', '+08:00')) = @yesterday
+
+GROUP BY DATE(w.created_at)
+ORDER BY DATE(w.created_at) DESC
+ON DUPLICATE KEY UPDATE
+    total_completed_amount = VALUES(total_completed_amount),
+    total_transactions = VALUES(total_transactions),
+    updated_at = CURRENT_TIMESTAMP;
+select * from uct_total_withdrawals_both_daily;
+
+
+select * from uct_total_withdrawals_both_weekly;
+SET @last_week_yearweek := YEARWEEK(CONVERT_TZ(DATE_SUB(NOW(), INTERVAL 1 WEEK), '+00:00', '+08:00'), 1);
+ 
+SET @cutoff := '2025-09-27 18:30:00';
+insert into uct_total_withdrawals_both_weekly(year_week,week_start_date,week_end_date,total_completed_amount, total_transactions)
+SELECT 
+    YEARWEEK(w.created_at, 1) AS year_week,
+    MIN(DATE(w.created_at)) AS week_start_date,
+    MAX(DATE(w.created_at)) AS week_end_date,
+    SUM(w.coins) AS total_completed_amount,
+    COUNT(*) AS total_transactions
+FROM gaming_app_backend.user_coin_transaction w
+JOIN gaming_app_backend.user u ON w.user = u.id
+WHERE w.created_at >= @cutoff and (w.user_coin_transaction_method = 3 or w.user_coin_transaction_method = 9)
+ AND YEARWEEK(CONVERT_TZ(w.created_at, '+00:00', '+08:00'), 1) = @last_week_yearweek
+GROUP BY YEARWEEK(w.created_at, 1)
+ORDER BY YEARWEEK(w.created_at, 1) DESC
+ON DUPLICATE KEY UPDATE 
+    total_completed_amount = VALUES(total_completed_amount),
+    total_transactions = VALUES(total_transactions),
+    updated_at = CURRENT_TIMESTAMP;
+
+select * from uct_total_withdrawals_both_weekly;
+
+
+select * from total_withdrawals_both_monthly;
+SET @last_month := DATE_FORMAT(CONVERT_TZ(DATE_SUB(NOW(), INTERVAL 1 MONTH), '+00:00', '+08:00'), '%Y-%m');
+
+SET @cutoff := '2025-09-27 18:30:00';
+insert into total_withdrawals_both_monthly(month_,total_completed_amount,total_transactions)
+SELECT 
+    DATE_FORMAT(w.created_at, '%Y-%m') AS month,
+    SUM(w.coins) AS total_completed_amount,
+    COUNT(*) AS total_transactions
+FROM gaming_app_backend.user_coin_transaction w
+JOIN gaming_app_backend.user u ON w.user = u.id
+WHERE w.created_at >= @cutoff and (w.user_coin_transaction_method = 3 or w.user_coin_transaction_method = 9)
+AND DATE_FORMAT(CONVERT_TZ(w.created_at, '+00:00', '+08:00'), '%Y-%m') = @last_month
+
+GROUP BY DATE_FORMAT(w.created_at, '%Y-%m')
+ORDER BY DATE_FORMAT(w.created_at, '%Y-%m') DESC
+ON DUPLICATE KEY UPDATE
+    total_completed_amount = VALUES(total_completed_amount),
+    total_transactions     = VALUES(total_transactions),
+    updated_at             = CURRENT_TIMESTAMP;
+select * from total_withdrawals_both_monthly;
+
+
+
+select * from total_withdrawals_both_total;
+
+-- SET @cutoff := '2025-09-27 18:30:00';
+-- insert into total_withdrawals_both_total(total_completed_amount, total_transactions)
+-- SELECT 
+-- SUM(w.coins) AS total_completed_amount,
+-- COUNT(*) AS total_transactions
+-- FROM gaming_app_backend.user_coin_transaction w
+-- JOIN gaming_app_backend.user u ON w.user = u.id
+-- WHERE w.created_at >= @cutoff and (w.user_coin_transaction_method = 3 or w.user_coin_transaction_method = 9);
+-- select * from total_withdrawals_both_total;
+
